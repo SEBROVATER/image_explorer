57a29ddf8fbb31a95c1eee6eb20e94835bbbd729
SEBROVATER <sebrovskiy.k@gmail.com>
2023-06-20 12:15:47 +0600

    any packages versions accepted

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 
 setup(
     name="image_inspector",
-    version="0.0.4",
+    version="0.0.5",
     packages=["image_inspector"],
     url="https://github.com/SEBROVATER/image_explorer",
     license="MIT",
     author="SEBROVATER",
     author_email="sebrovskiy.k@gmail.com",
     install_requires=[
-        "dearpygui>=1.9.1",
-        "numpy>=1.25.0",
-        "opencv-python>=4.7.0.72",
+        "dearpygui",
+        "numpy",
+        "opencv-python",
     ],
 )
